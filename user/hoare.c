8d914cdc37079136f8046b988b347f8c81db829b
Xian Wu <wuxian94@pku.edu.cn>
2015-12-08 20:32:08 +0800

    添加了调试开关

--- user/hoare.c
+++ user/hoare.c
@@ -8,7 +8,13 @@
 #define PROCSTART 0xAB010000
 #define PROCEND 0xABFF0000
 
-#define cprintf
+// #define debug
+
+#ifdef debug
+    #define dbg_cprintf cprintf
+#else
+    #define dbg_cprintf
+#endif
 
 /*
  * envid, A矩阵，定义见Hoare论文
@@ -84,17 +90,17 @@ wait_for_data (int rdc, struct RequiredData* rdv) {
 /*
 int
 example(void) {
-    cprintf("i am master environment %08x\n", thisenv->env_id);
+    dbg_cprintf("i am master environment %08x\n", thisenv->env_id);
     envid_t id = fork();
     if (id == 0) {
-        cprintf("i am child  environment %08x\n", thisenv->env_id);
+        dbg_cprintf("i am child  environment %08x\n", thisenv->env_id);
         struct RequiredData rd[2] = {
             {0x00001001, 0, 0},
             {0x00001001, 0, 0}
         };
         wait_for_data(2, rd);
-        cprintf("rd[0] = %d\n", rd[0].value);
-        cprintf("rd[1] = %d\n", rd[1].value);
+        dbg_cprintf("rd[0] = %d\n", rd[0].value);
+        dbg_cprintf("rd[1] = %d\n", rd[1].value);
         return 0;
     }
     ipc_send(id, 1, 0, 0);
@@ -171,18 +177,18 @@ north_proc(void) {
     if (who == env_id_mat[0][0] && (value & PROCMASK) == PROCSTART) {
         x = (value & 0xFF00) >> 8;
         y = (value & 0XFF);
-        cprintf("north_proc(%d, %d) %08x: started successfully.\n", x, y, thisenv->env_id);
+        dbg_cprintf("north_proc(%d, %d) %08x: started successfully.\n", x, y, thisenv->env_id);
     }
     else
         panic("north_proc: unexpected recv: %08x got %x from %08x\n", sys_getenvid(), value, who);
 
     // 进程主体
     while (count < NORDER) {
-        cprintf("north_proc(%d, %d) %08x: sending %d to center_proc(%d, %d) %08x.\n", x, y, thisenv->env_id, 0, x + 1, y, env_id_mat[x + 1][y]);
+        dbg_cprintf("north_proc(%d, %d) %08x: sending %d to center_proc(%d, %d) %08x.\n", x, y, thisenv->env_id, 0, x + 1, y, env_id_mat[x + 1][y]);
         ipc_send(env_id_mat[x + 1][y], 0, 0, 0);
         count++;
     }
-    cprintf("north_proc(%d, %d) %08x: return successfully.\n", x, y, thisenv->env_id);
+    dbg_cprintf("north_proc(%d, %d) %08x: return successfully.\n", x, y, thisenv->env_id);
     return 0;
 }
 
@@ -205,7 +211,7 @@ east_proc(void) {
     if (who == env_id_mat[0][0] && (value & PROCMASK) == PROCSTART) {
         x = (value & 0xFF00) >> 8;
         y = (value & 0XFF);
-        cprintf("east_proc(%d, %d) %08x: started successfully.\n", x, y, thisenv->env_id);
+        dbg_cprintf("east_proc(%d, %d) %08x: started successfully.\n", x, y, thisenv->env_id);
     }
     else
         panic("east_proc: unexpected recv: %x got %x from %x\n", sys_getenvid(), value, who);
@@ -237,11 +243,11 @@ east_proc(void) {
                 q_pop(&queue[i]);
             }
             // 数据处理
-            cprintf("east_proc(%d, %d) %08x: nothing to do.\n", x, y, thisenv->env_id);
+            dbg_cprintf("east_proc(%d, %d) %08x: nothing to do.\n", x, y, thisenv->env_id);
             count++;
         }
     }
-    cprintf("east_proc(%d, %d) %08x: return successfully.\n", x, y, thisenv->env_id);
+    dbg_cprintf("east_proc(%d, %d) %08x: return successfully.\n", x, y, thisenv->env_id);
     return 0;
 }
 
@@ -264,7 +270,7 @@ south_proc(void) {
     if (who == env_id_mat[0][0] && (value & PROCMASK) == PROCSTART) {
         x = (value & 0xFF00) >> 8;
         y = (value & 0XFF);
-        cprintf("south_proc(%d, %d) %08x: started successfully.\n", x, y, thisenv->env_id);
+        dbg_cprintf("south_proc(%d, %d) %08x: started successfully.\n", x, y, thisenv->env_id);
     }
     else
         panic("south_proc: unexpected recv: %x got %x from %x\n", sys_getenvid(), value, who);
@@ -296,12 +302,12 @@ south_proc(void) {
                 q_pop(&queue[i]);
             }
             // 数据处理
-            cprintf("south_proc(%d, %d) %08x: sending %d to collector(%d, %d) %08x.\n", x, y, thisenv->env_id, data[0], NDIMENSION - 1, NDIMENSION - 1, env_id_mat[NDIMENSION - 1][NDIMENSION - 1]);
+            dbg_cprintf("south_proc(%d, %d) %08x: sending %d to collector(%d, %d) %08x.\n", x, y, thisenv->env_id, data[0], NDIMENSION - 1, NDIMENSION - 1, env_id_mat[NDIMENSION - 1][NDIMENSION - 1]);
             ipc_send(env_id_mat[NDIMENSION - 1][NDIMENSION - 1], data[0], 0, 0);
             count++;
         }
     }
-    cprintf("south_proc(%d, %d) %08x: return successfully.\n", x, y, thisenv->env_id);
+    dbg_cprintf("south_proc(%d, %d) %08x: return successfully.\n", x, y, thisenv->env_id);
     return 0;
 }
 
@@ -324,7 +330,7 @@ west_proc(void) {
     if (who == env_id_mat[0][0] && (value & PROCMASK) == PROCSTART) {
         x = (value & 0xFF00) >> 8;
         y = (value & 0XFF);
-        cprintf("west_proc(%d, %d) %08x: started successfully.\n", x, y, thisenv->env_id);
+        dbg_cprintf("west_proc(%d, %d) %08x: started successfully.\n", x, y, thisenv->env_id);
     }
     else
         panic("west_proc: unexpected recv: %x got %d from %x\n", sys_getenvid(), value, who);
@@ -356,12 +362,12 @@ west_proc(void) {
                 q_pop(&queue[i]);
             }
             // 数据处理
-            cprintf("west_proc(%d, %d) %08x: sending %d to center_proc(%d, %d) %08x.\n", x, y, thisenv->env_id, data[0], x, y + 1, env_id_mat[x][y + 1]);
+            dbg_cprintf("west_proc(%d, %d) %08x: sending %d to center_proc(%d, %d) %08x.\n", x, y, thisenv->env_id, data[0], x, y + 1, env_id_mat[x][y + 1]);
             ipc_send(env_id_mat[x][y + 1], data[0], 0, 0);
             count++;
         }
     }
-    cprintf("west_proc(%d, %d) %08x: return successfully.\n", x, y, thisenv->env_id);
+    dbg_cprintf("west_proc(%d, %d) %08x: return successfully.\n", x, y, thisenv->env_id);
     return 0;
 }
 
@@ -384,7 +390,7 @@ center_proc(void) {
     if (who == env_id_mat[0][0] && (value & PROCMASK) == PROCSTART) {
         x = (value & 0xFF00) >> 8;
         y = (value & 0XFF);
-        cprintf("center_proc(%d, %d) %08x: started successfully.\n", x, y, thisenv->env_id);
+        dbg_cprintf("center_proc(%d, %d) %08x: started successfully.\n", x, y, thisenv->env_id);
     }
     else
         panic("center_proc: unexpected recv: %x got %x from %x\n", sys_getenvid(), value, who);
@@ -418,14 +424,14 @@ center_proc(void) {
             }
             // 数据处理
             value = A[x - 1][y - 1] * data[1] + data[0];
-            cprintf("center_proc(%d, %d) %08x: sending %d to proc(%d, %d) %08x.\n", x, y, thisenv->env_id, value, x + 1, y, env_id_mat[x + 1][y]);
+            dbg_cprintf("center_proc(%d, %d) %08x: sending %d to proc(%d, %d) %08x.\n", x, y, thisenv->env_id, value, x + 1, y, env_id_mat[x + 1][y]);
             ipc_send(env_id_mat[x + 1][y], value, 0, 0);
-            cprintf("center_proc(%d, %d) %08x: sending %d to proc(%d, %d) %08x.\n", x, y, thisenv->env_id, data[1], x, y + 1, env_id_mat[x][y + 1]);
+            dbg_cprintf("center_proc(%d, %d) %08x: sending %d to proc(%d, %d) %08x.\n", x, y, thisenv->env_id, data[1], x, y + 1, env_id_mat[x][y + 1]);
             ipc_send(env_id_mat[x][y + 1], data[1], 0, 0);
             count++;
         }
     }
-    cprintf("center_proc(%d, %d) %08x: return successfully.\n", x, y, thisenv->env_id);
+    dbg_cprintf("center_proc(%d, %d) %08x: return successfully.\n", x, y, thisenv->env_id);
     return 0;
 }
 
@@ -452,13 +458,13 @@ collector(void) {
     if (who == env_id_mat[0][0] && (value & PROCMASK) == PROCSTART) {
         x = (value & 0xFF00) >> 8;
         y = (value & 0XFF);
-        cprintf("collector(%d, %d) %08x: started successfully.\n", x, y, thisenv->env_id);
+        dbg_cprintf("collector(%d, %d) %08x: started successfully.\n", x, y, thisenv->env_id);
     }
     else
         panic("collector: unexpected recv: %x got %x from %x\n", sys_getenvid(), value, who);
 
     // 进程主体
-    cprintf("collector(%d, %d) %08x: collecting results.\n", x, y, thisenv->env_id);
+    dbg_cprintf("collector(%d, %d) %08x: collecting results.\n", x, y, thisenv->env_id);
     while (count < NORDER * NORDER) {
         value = ipc_recv(&who, 0, 0);
         for (i = 1; i <= NORDER; i++)
@@ -477,9 +483,9 @@ collector(void) {
     }
 
     value = PROCEND;
-    cprintf("collector(%d, %d) %08x: sending %d to master(%d, %d) %08x.\n", x, y, thisenv->env_id, value, 0, 0, env_id_mat[0][0]);
+    dbg_cprintf("collector(%d, %d) %08x: sending %d to master(%d, %d) %08x.\n", x, y, thisenv->env_id, value, 0, 0, env_id_mat[0][0]);
     ipc_send(env_id_mat[0][0], value, 0, 0);
-    cprintf("collector(%d, %d) %08x: return successfully.\n", x, y, thisenv->env_id);
+    dbg_cprintf("collector(%d, %d) %08x: return successfully.\n", x, y, thisenv->env_id);
 }
 
 /*
@@ -588,7 +594,7 @@ master(void) {
     value = ipc_recv(&who, 0, 0);
     if ((who != env_id_mat[NDIMENSION - 1][NDIMENSION - 1]) || ((value & PROCMASK) != PROCEND))
         panic("master: unexpected recv: %x got %x from %x\n", sys_getenvid(), value, who);
-    cprintf("master(%d, %d) %08x: return successfully.\n", 0, 0, thisenv->env_id);
+    dbg_cprintf("master(%d, %d) %08x: return successfully.\n", 0, 0, thisenv->env_id);
 }
 
 void
