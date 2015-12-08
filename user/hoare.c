376846e4036a4d01caedc258dbbdd4d79adb69e1
Xian Wu <wuxian94@pku.edu.cn>
2015-12-08 20:19:47 +0800

    添加了collector进程，用于消除拓扑图中的环

--- user/hoare.c
+++ user/hoare.c
@@ -1,11 +1,12 @@
 #include <inc/lib.h>
 
-#define NORDER 5
+#define NORDER 10
 #define NDIMENSION (NORDER + 2)
 #define MAXLENQUEUE (NORDER)
 
 #define PROCMASK 0xFFFF0000
 #define PROCSTART 0xAB010000
+#define PROCEND 0xABFF0000
 
 /*
  * envid, A矩阵，定义见Hoare论文
@@ -22,18 +23,28 @@ envid_t env_id_mat[NDIMENSION][NDIMENSION];
 //     {3, 8, 9}
 // };
 int A[NORDER][NORDER] = {
-    {1, 5, 6, 1, 1},
-    {2, 5, 7, 1, 1},
-    {2, 1, 4, 1, 1},
-    {1, 1, 1, 1, 1},
-    {1, 1, 1, 1, 1}
+    {1, 5, 6, 1, 1, 1, 5, 6, 1, 1},
+    {2, 5, 7, 1, 1, 1, 5, 6, 1, 1},
+    {2, 1, 4, 1, 1, 1, 5, 6, 1, 1},
+    {1, 1, 1, 1, 1, 1, 5, 6, 1, 1},
+    {1, 1, 1, 1, 1, 1, 5, 6, 1, 1},
+    {1, 5, 6, 1, 1, 1, 5, 6, 1, 1},
+    {2, 5, 7, 1, 1, 1, 5, 6, 1, 1},
+    {2, 1, 4, 1, 1, 1, 5, 6, 1, 1},
+    {1, 1, 1, 1, 1, 1, 5, 6, 1, 1},
+    {1, 1, 1, 1, 1, 1, 5, 6, 1, 1}
 };
 int B[NORDER][NORDER] = {
-    {1, 2, 7, 1, 1},
-    {4, 5, 1, 1, 1},
-    {3, 8, 9, 1, 1},
-    {1, 1, 1, 1, 1},
-    {1, 1, 1, 1, 1}
+    {1, 2, 7, 1, 1, 1, 2, 7, 1, 1},
+    {4, 5, 1, 1, 1, 1, 2, 7, 1, 1},
+    {3, 8, 9, 1, 1, 1, 2, 7, 1, 1},
+    {1, 1, 1, 1, 1, 1, 2, 7, 1, 1},
+    {1, 1, 1, 1, 1, 1, 2, 7, 1, 1},
+    {1, 2, 7, 1, 1, 1, 2, 7, 1, 1},
+    {4, 5, 1, 1, 1, 1, 2, 7, 1, 1},
+    {3, 8, 9, 1, 1, 1, 2, 7, 1, 1},
+    {1, 1, 1, 1, 1, 1, 2, 7, 1, 1},
+    {1, 1, 1, 1, 1, 1, 2, 7, 1, 1}
 };
 int C[NORDER][NORDER];
 
@@ -283,8 +294,8 @@ south_proc(void) {
                 q_pop(&queue[i]);
             }
             // 数据处理
-            cprintf("south_proc(%d, %d) %08x: sending %d to master(%d, %d) %08x.\n", x, y, thisenv->env_id, data[0], 0, 0, env_id_mat[0][0]);
-            ipc_send(env_id_mat[0][0], data[0], 0, 0);
+            cprintf("south_proc(%d, %d) %08x: sending %d to collector(%d, %d) %08x.\n", x, y, thisenv->env_id, data[0], NDIMENSION - 1, NDIMENSION - 1, env_id_mat[NDIMENSION - 1][NDIMENSION - 1]);
+            ipc_send(env_id_mat[NDIMENSION - 1][NDIMENSION - 1], data[0], 0, 0);
             count++;
         }
     }
@@ -417,6 +428,59 @@ center_proc(void) {
 }
 
 /*
+ * 结果收集进程
+ */
+void
+collector(void) {
+    // 数据定义部分
+    const int NDATA = 2;    // 需求数据数目
+    int i, j;               // 循环变量
+    int x, y;               // 当前proc的env_id在env_id_mat中的位置
+    int value, count = 0;   // 数据和获得数据计数
+    envid_t who;            // 信息来源
+    envid_t from[NDATA];    // 需求数据来源
+    int data[NDATA];        // 一轮中收集的数据
+    struct Queue queue[NDATA];  // 数据队列
+    int ready = 1;          // 是否收集了一轮所需的数据
+    int index[NORDER] = {0};
+
+    // 进程初始化
+    // 从master获得PROCSTART信号，并设置x, y
+    value = ipc_recv(&who, 0, 0);
+    if (who == env_id_mat[0][0] && (value & PROCMASK) == PROCSTART) {
+        x = (value & 0xFF00) >> 8;
+        y = (value & 0XFF);
+        cprintf("collector(%d, %d) %08x: started successfully.\n", x, y, thisenv->env_id);
+    }
+    else
+        panic("collector: unexpected recv: %x got %x from %x\n", sys_getenvid(), value, who);
+
+    // 进程主体
+    cprintf("collector(%d, %d) %08x: collecting results.\n", x, y, thisenv->env_id);
+    while (count < NORDER * NORDER) {
+        value = ipc_recv(&who, 0, 0);
+        for (i = 1; i <= NORDER; i++)
+            if (who == env_id_mat[NDIMENSION - 1][i])
+                break;
+        if (i == NORDER + 1)
+            panic("collector: unexpected recv: %08x got %x from %08x\n", sys_getenvid(), value, who);
+        C[index[i-1]++][i-1] = value; // receive from different stage stream!!区分不同阶段计算出来的值
+        count++;
+    }
+
+    for (i = 0; i < NORDER; i++) {
+        for (j = 0; j < NORDER; j++)
+            cprintf("%d ", C[i][j]);
+        cprintf("\n");
+    }
+
+    value = PROCEND;
+    cprintf("collector(%d, %d) %08x: sending %d to master(%d, %d) %08x.\n", x, y, thisenv->env_id, value, 0, 0, env_id_mat[0][0]);
+    ipc_send(env_id_mat[0][0], value, 0, 0);
+    cprintf("collector(%d, %d) %08x: return successfully.\n", x, y, thisenv->env_id);
+}
+
+/*
  * 主控制进程
  */
 void
@@ -424,7 +488,6 @@ master(void) {
     int i, j;
     int value, count = 0;
     envid_t env_id = 0x00001001, who;
-    int index[NORDER] = {0};
     env_id_mat[0][0] = thisenv->env_id;
 
     // env_id_mat需要被提前设置，否则后面fork出的程序无法得到其信息
@@ -433,6 +496,7 @@ master(void) {
     for (i = NORDER; i >=1; i--) for (j = NORDER; j >=1; j--) env_id_mat[i][j] = ++env_id;
     for (i = 1; i <= NORDER; i++) env_id_mat[0][i] = ++env_id;
     for (i = 1; i <= NORDER; i++) env_id_mat[i][0] = ++env_id;
+    env_id_mat[NDIMENSION - 1][NDIMENSION - 1] = ++env_id;
 
     for (i = 1; i <= NORDER; i++) {
         envid_t id = fork();
@@ -480,6 +544,13 @@ master(void) {
         if (id != env_id_mat[i][0])
             panic("unmatched env id: proc(%d, %d) %08x.", i, 0, id);
     }
+    envid_t id = fork();
+    if (id == 0) {
+        collector();
+        return;
+    }
+    if (id != env_id_mat[NDIMENSION - 1][NDIMENSION - 1])
+        panic("unmatched env id: proc(%d, %d) %08x.", NDIMENSION - 1, NDIMENSION - 1, id);
 
     // 启动所有进程
     // 要严格按照这个启动顺序，否则会panic
@@ -504,29 +575,18 @@ master(void) {
         value = PROCSTART | i << 8 | 0;
         ipc_send(env_id_mat[i][0], value, 0, 0);
     }
+    value = PROCSTART | (NDIMENSION - 1) << 8 | (NDIMENSION - 1);
+    ipc_send(env_id_mat[NDIMENSION - 1][NDIMENSION - 1], value, 0, 0);
 
     for (i = 1; i <= NORDER; i++)
         for (j = 1; j <= NORDER; j++) {
             ipc_send(env_id_mat[j][0], B[i-1][j-1], 0, 0);
         }
 
-    cprintf("master(%d, %d) %08x: collecting results.\n", 0, 0, thisenv->env_id);
-    while (count < NORDER * NORDER) {
-        value = ipc_recv(&who, 0, 0);
-        for (i = 1; i <= NORDER; i++)
-            if (who == env_id_mat[NDIMENSION - 1][i])
-                break;
-        if (i == NORDER + 1)
-            panic("master: unexpected recv: %08x got %x from %08x\n", sys_getenvid(), value, who);
-        C[index[i-1]++][i-1] = value; // receive from different stage stream!!区分不同阶段计算出来的值
-        count++;
-    }
-
-    for (i = 0; i < NORDER; i++) {
-        for (j = 0; j < NORDER; j++)
-            cprintf("%d ", C[i][j]);
-        cprintf("\n");
-    }
+    value = ipc_recv(&who, 0, 0);
+    if ((who != env_id_mat[NDIMENSION - 1][NDIMENSION - 1]) || ((value & PROCMASK) != PROCEND))
+        panic("master: unexpected recv: %x got %x from %x\n", sys_getenvid(), value, who);
+    cprintf("master(%d, %d) %08x: return successfully.\n", 0, 0, thisenv->env_id);
 }
 
 void
